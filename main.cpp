d9c16cba081d5e6441780f44035fff87887c903f
lwalsh6 <lwalsh6@ut.utm.edu>
2022-12-01 19:10:37 -0600

    Incomplete tests added

--- main.cpp
+++ main.cpp
@@ -13,8 +13,40 @@
 using namespace std;
 
 int main() {
-  deque test;
-  for (int i = 0; i < 100; i++){
+  deque dq;
+  cout << "TESTING: push_back" << endl;
+  for (int i = 0; i < 5000; i++){
+    dq.push_back(i);
+  }
+  cout << endl;
+
+  cout << "TESTING: push_front" << endl;
+  for (int i = 5000; i < 0; i--){
+    dq.push_front(i);
+  }
+  cout << endl;
+
+  cout << "TESTING: [] Overload" << endl;;
+  for (int i = 0; i < 5000; i+=100){
+    cout << dq[i] << ' ';
+  }
+  cout << endl;
+  
+  cout << "TESTING: pop_back" << endl;
+  for (int i = 0; i < 5000; i++){
+    dq.pop_back();
+  }
+  cout << endl;
+
+  cout << "TESTING: pop_front" << endl;
+  for (int i = 1; i < 5000; i++){
+    dq.pop_front();
+  }
+  cout << endl;
+
+  
+
+  /*for (int i = 0; i < 100; i++){
     test.push_back(i);
   }
 
@@ -34,6 +66,6 @@ int main() {
   cout << test[i] << " ";
 }
   cout << endl;
-  return 0;
+  return 0;*/
 }
 
